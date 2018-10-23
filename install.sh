bb24face9f6e64a89e9f1019f9a7eda3bee34690
l0ui3 <hos2002820@hotmail.com>
2018-10-23 17:41:40 +0800

    remove _dot in filename

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Setup .bash prompt and aliases
-cp dot_bash_profile ~/.bash_profile
-cp dot_bash_prompt ~/.bash_prompt
-cp dot_bash_aliases ~/.bash_aliases
+cp bash_profile ~/.bash_profile
+cp bash_prompt ~/.bash_prompt
+cp bash_aliases ~/.bash_aliases
 
 # Vim config
-cp dot_vimrc ~/.vimrc
+cp vimrc ~/.vimrc
 
 # SSH config
-cp dot_ssh_config ~/.ssh/config
+cp ssh_config ~/.ssh/config
 
 # TMUX config
-cp dot_tmux_conf ~/.tmux.conf
+cp tmux_conf ~/.tmux.conf
 
 # Install Homebrew (Interactive)
 if [[ ! -e /usr/local/Homebrew ]]; then
