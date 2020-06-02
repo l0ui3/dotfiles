05fe2d31e1fded3b32fba2e46d6dd9facefe02cc
Louie <louie@PS-Louies-iMac.local>
2020-06-02 15:17:53 +0800

    fix font rendering in Catalina

--- install_mac.sh
+++ install_mac.sh
@@ -21,9 +21,48 @@ fi
 # Install packages by brew bundle
 brew bundle
 
-# Fix font render in Mojave
-defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
-defaults -currentHost write -globalDomain AppleFontSmoothing -int 1
+# Fix font render in Mojave (Don't use this in Catalina)
+#defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+#defaults -currentHost write -globalDomain AppleFontSmoothing -int 1
+
+# Reset FontSmoothing
+defaults -currentHost delete -globalDomain AppleFontSmoothing
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool True
+
+# Show full bundle identifier of a package
+# mdls -name kMDItemCFBundleIdentifier /Applications/Hyper.app
+
+# Fix Hyper Terminal Font
+defaults write co.zeit.hyper CGFontRenderingFontSmoothingDisabled 0
+defaults write co.zeit.hyper.helper CGFontRenderingFontSmoothingDisabled 0
+defaults write co.zeit.hyper.helper.EH CGFontRenderingFontSmoothingDisabled 0
+defaults write co.zeit.hyper.helper.NP CGFontRenderingFontSmoothingDisabled 0
+
+defaults write com.microsoft.VSCode CGFontRenderingFontSmoothingDisabled 0
+defaults write com.microsoft.VSCode.helper CGFontRenderingFontSmoothingDisabled 0
+defaults write com.microsoft.VSCode.helper.EH CGFontRenderingFontSmoothingDisabled 0
+defaults write com.microsoft.VSCode.helper.NP CGFontRenderingFontSmoothingDisabled 0
+
+defaults write com.apple.Notes CGFontRenderingFontSmoothingDisabled 0
+defaults write com.apple.Notes CGFontRenderingFontSmoothingDisabled 0
+defaults write com.apple.Notes CGFontRenderingFontSmoothingDisabled 0
+defaults write com.apple.Notes CGFontRenderingFontSmoothingDisabled 0
+
+defaults write com.sublimetext.3 CGFontRenderingFontSmoothingDisabled 0
+defaults write com.sublimetext.3 CGFontRenderingFontSmoothingDisabled 0
+defaults write com.sublimetext.3 CGFontRenderingFontSmoothingDisabled 0
+defaults write com.sublimetext.3 CGFontRenderingFontSmoothingDisabled 0
+
+defaults write com.spotify.client CGFontRenderingFontSmoothingDisabled 0
+defaults write com.spotify.client CGFontRenderingFontSmoothingDisabled 0
+defaults write com.spotify.client CGFontRenderingFontSmoothingDisabled 0
+defaults write com.spotify.client CGFontRenderingFontSmoothingDisabled 0
+
+defaults write com.apple.Terminal CGFontRenderingFontSmoothingDisabled 0
+defaults write com.apple.Terminal CGFontRenderingFontSmoothingDisabled 0
+defaults write com.apple.Terminal CGFontRenderingFontSmoothingDisabled 0
+defaults write com.apple.Terminal CGFontRenderingFontSmoothingDisabled 0
+
 
 # Change app language
 defaults write com.apple.iCal AppleLanguages '("zh-TW")'
