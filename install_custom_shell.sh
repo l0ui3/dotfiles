da1e4388f2486cba8f8589039114d1178e839d3e
l0ui3 <hos2002820@gmail.com>
2021-07-02 13:47:25 +0800

    add zsh plugin
    - auto activate python virtualenv

--- install_custom_shell.sh
+++ install_custom_shell.sh
@@ -49,4 +49,5 @@ sed -i '' '1s/^/# Sleep a short time to prevent showing % sign\nsleep 0.5\n/' ~/
 
 # Install Oh-My-ZSH plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i '' "s/^plugins=(.*)$/plugins=(git python docker osx zsh-autosuggestions autojump)/g" ~/.zshrc
+git clone https://github.com/RobertDeRose/virtualenv-autodetect.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/virtualenv-autodetect
+sed -i '' "s/^plugins=(.*)$/plugins=(git python docker osx zsh-autosuggestions autojump virtualenv-autodetect)/g" ~/.zshrc
