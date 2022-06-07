c8d8ff99208aa114a73cfa0515db2be9a2c46f2e
Louie Jhuang <1160374+l0ui3@users.noreply.github.com>
2022-06-07 11:58:38 +0800

    feat: add a lite version of custom shell

---
+++ custom_zsh_lite.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# This script is intended to install a more handy shell and custom flavor for server management.
+# Tested on Ubuntu server.
+
+
+# exit when any command fails
+set -e
+
+sudo apt update -y
+sudo apt install -y zsh
+
+# Install Oh My ZSH
+if [[ ! -e ~/.oh-my-zsh ]]; then
+    echo "[*] Ready to install Oh-My-Zsh"
+    echo "[!] Please re-run this script after Oh-My-Zsh is installed."
+    read -p "Press Enter key to continue..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    exit
+fi
+
+# Install Oh-My-ZSH plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/RobertDeRose/virtualenv-autodetect.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/virtualenv-autodetect
+sed -i "s/^plugins=(.*)$/plugins=(git docker zsh-autosuggestions virtualenv-autodetect)/g" ~/.zshrc
+
+# Change ZSH theme
+sed -i "s/^ZSH_THEME.*/ZSH_THEME=bira/g" ~/.zshrc
+
+# Change ZSH autosuggestion color to fit OneDark theme
+echo "# ZSH autosuggestion color" >> ~/.zshrc
+echo "ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#757575'" >> ~/.zshrc
+
+# Add kubectl auto-completion
+echo "" >> ~/.zshrc
+echo "# ZSH Auto-Completion" >> ~/.zshrc
+echo 'source <(kubectl completion zsh)' >> ~/.zshrc
